5936c6387a3f53bc0c44e54712b74124e5bc10b9
Emily Underhill <emilyru93@gmail.com>
2021-11-11 14:24:52 +0000

    routes added

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ movies.each do |movie|
   Movie.create(
     title: movie['title'],
     overview: movie['overview'],
-    poster_url: "https://image.tmdb.org/t/p/w500#{poster_path}",
+    poster_url: "https://image.tmdb.org/t/p/original#{poster_path}",
     rating: movie['vote_average']
   )
 end
