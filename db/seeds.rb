838b2b6d24400c8842964acb72c306d54927d96d
Emily Underhill <emilyru93@gmail.com>
2021-11-11 13:12:27 +0000

    movie seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Creating movies'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+results = JSON.parse(URI.open(url).read)
+movies = results['results']
+
+movies.each do |movie|
+  poster_path = movie['poster_path']
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{poster_path}",
+    rating: movie['vote_average']
+  )
+end
+
+puts 'Finished!'
